33ad604d3c5ab6fc590b6dc9a7e4386dcea7a4d3
Lavinia <laviniacristina.botea@ulbsibiu.ro>
2026-03-24 11:10:19 +0200

    lab4

---
+++ .idea/Tanar.java
@@ -0,0 +1,23 @@
+package Lab4;
+
+public class Tanar {
+    private String name;
+    private int age;
+    private String address;
+
+    public Tanar(String numenou, int agenou, String adresanoua) {
+        this.name = numenou;
+        this.age = agenou;
+        this.address = adresanoua;
+    }
+
+    @Override
+    public String toString() {
+        return "Tanar{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+}
---
+++ src/AppLab4.java
@@ -0,0 +1,51 @@
+import Lab4.Tanar;
+
+void main() {
+    HashMap<String, Integer> varste = new HashMap<>();
+    varste.put("Ioan", 21);
+    varste.put("Maria", 22);
+    varste.put("Victor", 20);
+    varste.put("Simina", 20);
+    varste.put("Marius", 21);
+    varste.put("Mihai", 21);
+    varste.put("Daniela", 23);
+
+    Map<String, String> adrese = Map.of("Ioan", "Sibiu", "Maria", "Bucuresti", "Victor",
+            "Cluj","Simina", "Alba-Iulia","Marius", "Medias", "Mihai", "Cisnadie","Daniela", "Sibiu");
+
+
+    //primul MAP
+    for(Object key : varste.keySet())
+    {
+        Object value = varste.get(key);
+        IO.println("Nume: " + key + "    " + " Varsta: "+ value);
+    }
+
+    IO.println();
+    IO.println();
+//Primul MAP dupa adaugare
+    varste.put("Vlad", 19);
+    varste.put("Iulia", 19);
+
+    for(Object key : varste.keySet())
+    {
+        Object value = varste.get(key);
+        IO.println("Nume: " + key + "    " + " Varsta: "+ value);
+    }
+
+
+   //Al doilea MAP
+    IO.println();
+    IO.println();
+
+    for(Object key : adrese.keySet())
+    {
+        Object value = adrese.get(key);
+        IO.println("Nume: " + key + "    " + " Adresa: "+ value);
+    }
+
+
+    HashMap<String, Tanar> tineri;
+    tineri.put(das hyu);
+
+}
